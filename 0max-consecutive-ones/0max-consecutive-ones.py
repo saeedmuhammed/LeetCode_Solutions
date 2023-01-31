364b2869eb0f577228f112eac3d0ce6262fcf1ad
Saeed Muhammed <36425133+saeedmuhammed@users.noreply.github.com>
2023-01-31 21:52:50 +0200

    Time: 267 ms, Memory: 13.5 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.py
+++ 0max-consecutive-ones/0max-consecutive-ones.py
@@ -4,13 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = 0
-        final = 0
-        for i in nums:
-            if i == 1:
-                count+=1
-            else:
-                count=0
-            if count > final :
-                final = count
-        return final 
+        
+        return len(max("".join(map(str,nums)).split("0")))
