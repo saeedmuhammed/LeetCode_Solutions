a659b874888d49bd1cd07613c11d4b828a67c544
Saeed Muhammed <36425133+saeedmuhammed@users.noreply.github.com>
2023-03-30 03:43:05 +0200

    Time: 269 ms (83.19%), Space: 64.6 MB (58.95%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -1,21 +1,17 @@
 /**
  * @param {number[]} temperatures
  * @return {number[]}
+ This solution with stack
  */
 var dailyTemperatures = function(temperatures) {
-    let outputt = [];
-    for (let i = 0 ; i<temperatures.length ; i++){
-        let count = 0;
-        let found = false;
-        for (let j = i+1 ; j < temperatures.length ; j++){
-            count++;
-            if(temperatures[i] < temperatures[j]){
-               found=true;
-                break;
-            }
-            
+    let output = new Array(temperatures.length).fill(0);
+    let stack = [];
+    for (let i = 0 ; i < temperatures.length ; i++){
+        while (stack.length != 0  && temperatures[i] > temperatures[stack[stack.length-1]]){
+            let temp = stack.pop();
+            output[temp] = i - temp ;
         }
-        found ? outputt.push(count) :outputt.push(0);
+        stack.push(i);
     }
-    return outputt;
+    return output;
 };
